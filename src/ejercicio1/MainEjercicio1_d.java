b5669511ce0b86778338cb8aaae975baa7b1ffcf
Macarena Hirn <macarenahirn@gmail.com>
2025-09-03 00:46:02 -0300

    Cambio DNI

--- src/ejercicio1/MainEjercicio1_d.java
+++ src/ejercicio1/MainEjercicio1_d.java
@@ -4,11 +4,11 @@ import java.util.HashSet;
 
 public class MainEjercicio1_d {
 	 public static void main(String[] args) {
-		 Persona P1 = new Persona("12345678A", "Ramiro", "Gomez", LocalDate.of(1990, 5, 12), "Masculino", "Calle Salsa 123, Ciudad", "01112345678", "ramiro.gomez@email.com");
-		 Persona P2 = new Persona("87654321B", "Lucia", "Gonzalez", LocalDate.of(1985, 8, 23), "Femenino", "Av. Siempre Viva 742, Ciudad", "01187654321", "lucia.gonzalez@email.com");
-		 Persona P3 = new Persona("11223344C", "Sofia", "Martinez", LocalDate.of(2000, 2, 10), "Femenino", "Calle Libertad 45, Ciudad", "01111223344", "sofia.martinez@email.com");
-		 Persona P4 = new Persona("44332211D", "Carlos", "Ramirez", LocalDate.of(1995, 11, 30), "Masculino", "Boulevard Central 98, Ciudad", "01144332211", "carlos.ramirez@email.com");
-		 Persona P5 = new Persona("55667788E", "Valentina", "Lopez", LocalDate.of(1992, 7, 5), "Femenino", "Calle del Sol 77, Ciudad", "01155667788", "valentina.lopez@email.com");
+		 Persona P1 = new Persona("12345678", "Ramiro", "Gomez", LocalDate.of(1990, 5, 12), "Masculino", "Calle Salsa 123, Ciudad", "01112345678", "ramiro.gomez@email.com");
+		 Persona P2 = new Persona("87654321", "Lucia", "Gonzalez", LocalDate.of(1985, 8, 23), "Femenino", "Av. Siempre Viva 742, Ciudad", "01187654321", "lucia.gonzalez@email.com");
+		 Persona P3 = new Persona("11223344", "Sofia", "Martinez", LocalDate.of(2000, 2, 10), "Femenino", "Calle Libertad 45, Ciudad", "01111223344", "sofia.martinez@email.com");
+		 Persona P4 = new Persona("44332211", "Carlos", "Ramirez", LocalDate.of(1995, 11, 30), "Masculino", "Boulevard Central 98, Ciudad", "01144332211", "carlos.ramirez@email.com");
+		 Persona P5 = new Persona("55667788", "Valentina", "Lopez", LocalDate.of(1992, 7, 5), "Femenino", "Calle del Sol 77, Ciudad", "01155667788", "valentina.lopez@email.com");
 
 		 
 		 HashSet<Persona> ListaPersonas = new HashSet<Persona>();
