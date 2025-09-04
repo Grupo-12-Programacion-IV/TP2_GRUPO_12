8580695174309946fa38bd2aaf2edc7a8d478487
sofianeduk <sofianeduk2013@gmail.com>
2025-09-04 18:01:00 -0300

    aaa

--- src/ejercicio2/Edificio.java
+++ src/ejercicio2/Edificio.java
@@ -2,4 +2,18 @@ package ejercicio2;
 
 public class Edificio {
 
+	private double superficie;
+	
+	public Edificio() {};
+	
+	public Edificio (double superficie) {
+		this.superficie=superficie;
+	}
+	
+	public double getSuperficieEdificio() {
+		return superficie;
+	}
+
+	
+	
 }
